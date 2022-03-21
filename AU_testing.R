1169d213944d05c27fef0222c332e847d0ec303f
Andrew Uen <andrewruen@gmail.com>
2022-03-21 16:42:43 +1100

    initial model testing

---
+++ AU_testing.R
@@ -0,0 +1,24 @@
+setwd("~/Desktop/control-cycle")
+source("00 set up.R")
+source("01 cleanup.R")
+
+head(league_defense)
+View(league_shooting)
+
+table1 = tournament_shooting %>%
+  filter(Nation=="Dosqaly", Year=="2020", Pos!="DF", Pos!="GK") 
+View(table1)
+table1a = league_shooting %>%
+  filter(Nation=="Dosqaly", Year=="2020", Pos!="DF", Pos!="GK") 
+View(table1a)
+
+
+table2 = tournament_shooting %>%
+  filter(Nation=="Dosqaly", Year=="2020") 
+View(table2)
+table2 = tournament_shooting %>%
+  filter(Nation=="Sobianitedrucy", Year=="2021", Pos!="DF") 
+View(table2)
+
+
+#!str_detect(Pos, "DF")
